74e69d7022215d4d6228ddf207d2584c306e1d02
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2022-06-13 16:27:38 -0300

    Agregado de templates de pagina de inicio//Add templates index page

--- appStore/urls.py
+++ appStore/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio')
+    path('', views.inicio, name='inicio'),
+    path('articulos', views.articulos, name='articulos'),
+    path('verarticulos',views.verarticulos,name='verarticulos'),
 ]
 
--- appStore/views.py
+++ appStore/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 #posteriormente para poder ver las vistas, tenemos que crear el archivo urls.py
 def inicio(request):
-    return HttpResponse("<h1>Creando la primera vista</h1>")
+    return render(request, 'paginas/inicio.html')
+
+def articulos(request):
+    return render(request, 'paginas/articulos.html')
+
+def verarticulos(request):
+    return render(request, 'articulos/index.html')
+
+
+
+    
