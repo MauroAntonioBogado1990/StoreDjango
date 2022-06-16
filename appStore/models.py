925fb2cc86c0f20d42f1ef7c1435aab438ec95fa
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2022-06-15 23:30:54 -0300

    Create models and migrate

--- appStore/models.py
+++ appStore/models.py
@@ -1,3 +1,13 @@
+from dataclasses import field
+from distutils.command import upload
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class Articulo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=250)
+    precio = models.IntegerField()
+    imagen = models.ImageField(upload_to = 'imagenes/', verbose_name="Imagen", null=True)
+    descripcion = models.TextField(max_length=500, verbose_name="Descripcion")                
+                   
