93208d031bf29f2691c715599f08c14a25a698fc
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2022-08-22 21:17:11 -0300

    Adpatacion para que pueda funcionar en el servidor

--- appStore/models.py
+++ appStore/models.py
@@ -9,7 +9,7 @@ class Articulo(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=250)
     precio = models.IntegerField()
-    imagen = models.ImageField(upload_to = 'imagenes/', verbose_name="Imagen", null=True)
+    #imagen = models.ImageField(upload_to = 'imagenes/', verbose_name="Imagen", null=False, blank=True)
     descripcion = models.TextField(max_length=500, verbose_name="Descripcion")                
                    
     def __str__(self) -> str:
@@ -17,5 +17,5 @@ class Articulo(models.Model):
         return fila
     #eliminacion del registro
     def delete(self, using=None, keep_parents=False):
-        self.imagen.storage.delete(self.imagen.name)   
+        #self.imagen.storage.delete(self.imagen.name)   
         super().delete()
